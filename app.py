cfefcbfed586253f623ead9d47e1309491c933c4
syazanaroslimi <nrlsyazana69@gmail.com>
2025-10-31 19:59:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.set_page_config(
+  page_title = "Crimes Against Women in India"
+)
+
+visualise = st.Page('page1.py', title='Objective 1')
+
+pg=st.navigation(
+  {
+    "Menu":[visualise]
+  }
+)
+
+pg.run()
